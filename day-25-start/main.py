6eddb981d09b779eb0cd2af2136f39b3651f682d
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-09-24 22:51:05 +0530

    Update main.py
    pandas library different function to manipulate with csv files

--- day-25-start/main.py
+++ day-25-start/main.py
@@ -12,9 +12,40 @@
 #             temperatures.append(row[1])
 #     print(temperatures)
 
+# import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
+# print(data)
+# print(data['temp'])
+
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
+print(type(data))
 print(data)
-print(data['temp'])
+print(type(data["temp"]))
+print(data["temp"])
 
+data_dict = data.to_dict()
+temp_list = data["temp"].to_list()
+
+print(data_dict)
+print(temp_list)
+
+mean = data["temp"].mean()
+average = sum(temp_list) / len(temp_list)
+print(average)
+print(mean)
+print(data["temp"].max())
+print(data["temp"].min())
+
+print(data["condition"])
+print(data.condition)
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+monday_temp = monday.temp[0]
+monday_temp_f = monday_temp * 9/5 + 32
+
+print(monday_temp_f)
