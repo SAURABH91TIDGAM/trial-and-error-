e98ecb0e5141316d047d209235d6cd05780c16ad
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-05 20:48:50 +0530

    Update main.py

--- pythonProject-day27/main.py
+++ pythonProject-day27/main.py
@@ -1,12 +1,15 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold", "italic"))
+my_label = Label(text="I am a label", font=("Arial", 24, "bold", "italic"))
 my_label.pack(side="left")
 
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
 
+button = Button()
 
 window.mainloop()
