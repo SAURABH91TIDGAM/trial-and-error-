5c1e20b4375a45ecfd36e5853838896c1f46c8a5
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-05 20:33:39 +0530

    day 27 arguments
    multiple key arguments in a function

--- pythonProject-day27/playground.py
+++ pythonProject-day27/playground.py
@@ -8,3 +8,27 @@ def add(*args):
     return sum
 
 print(add(3, 5, 6, 7, 8, 4, 2))
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    print(kwargs["add"])
+
+    n += kwargs["add"]
+    n +=kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3, multiply=5)
+
+class Car:
+    def __init__(self, **kwargs):
+        self.make = kwargs.get("make")
+        self.model = kwargs.get("model")
+        self.color = kwargs.get("color")
+
+my_car = Car(make="nissan")
+
+print(my_car.model)
+print(my_car.make)
