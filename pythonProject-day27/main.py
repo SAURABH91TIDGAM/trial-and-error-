392bd60598f400d9eb813aae0f78e8ba8ec6d957
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-06 21:18:08 +0530

    substitutes of pack()
    grid and placed instead of pack

--- pythonProject-day27/main.py
+++ pythonProject-day27/main.py
@@ -4,29 +4,32 @@ window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
+
 my_label = Label(text="I am a label", font=("Arial", 24, "bold", "italic"))
+my_label.grid(column=0, row=0)
 #my_label.pack(side="top")
-my_label.place(x=0, y=0)
+
 
 my_label["text"] = "New Text"
-my_label.config(text="placed at the top left ")
+my_label.config(text="one")
 
 input = Entry()
 
 def button_clicked():
     # print("I got clicked")
 
-    my_label.config(text=input.get())
+   my_label.config(text=input.get())
 
 
 button = Button(text="print text", command=button_clicked)
-input.pack()
-button.pack()
+input.grid(column=4, row=4)
+# button.pack()
+button.grid(column=3, row=3)
 
 
 #Entry
 
-# input = Entry()
+input = Entry()
 # input.pack()
 # print(input.get())
 
