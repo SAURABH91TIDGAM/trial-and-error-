edbcb26e96704b3db7fa35006f19bac096a5ae35
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-06 20:51:51 +0530

    tkinter widgets

--- pythonProject-day27/main.py
+++ pythonProject-day27/main.py
@@ -5,11 +5,30 @@ window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
 my_label = Label(text="I am a label", font=("Arial", 24, "bold", "italic"))
-my_label.pack(side="left")
+my_label.pack(side="top")
 
 my_label["text"] = "New Text"
 my_label.config(text="New Text")
 
-button = Button()
+input = Entry()
+
+def button_clicked():
+    # print("I got clicked")
+
+    my_label.config(text=input.get())
+
+
+button = Button(text="print text", command=button_clicked)
+input.pack()
+button.pack()
+
+
+#Entry
+
+# input = Entry()
+# input.pack()
+# print(input.get())
+
+
 
 window.mainloop()
