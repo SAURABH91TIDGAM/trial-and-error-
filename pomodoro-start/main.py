c65ef4dd6c6efe38a18d83ab1fb98fa3aa2ee995
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-10 21:01:23 +0530

    Update main.py
    timer mechanism
    button functions

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -10,10 +11,23 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def stop_timer():
+    print("hello")
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+
+    count_down(25 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_sec = 00
+    count_min = 00
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -22,22 +36,25 @@ window.title("Pomodoro")
 window.minsize(width=500, height=500)
 window.config(padx=90, pady=100, bg=YELLOW)
 
+
+
 timer = Label(text="Timer", font=("Arial", 30, "italic"), bg=YELLOW, fg=GREEN)
 timer.grid(row=0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 112, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 112, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-start = Label(text="Start", font=("Arial", 12, "italic"), bg=YELLOW)
-start.grid(row=2, column=0)
+
+start_button = Button(text="Start", font=("Arial", 12, "italic"), bg=YELLOW, command=start_timer)
+start_button.grid(row=2, column=0)
 
 tick = Label(text="✔", font=("Arial", 12, "italic"), bg=YELLOW, fg=GREEN)
 tick.grid(row=3, column=1)
 
-stop = Label(text="Stop", font=("Arial", 12, "italic"), bg=YELLOW)
-stop.grid(row=2, column=2)
+stop_button = Button(text="Stop", font=("Arial", 12, "italic"), bg=YELLOW, command=stop_timer)
+stop_button.grid(row=2, column=2)
 
 window.mainloop()
