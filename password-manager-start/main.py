d797dc151d93a0cf33fb824003841c92a5603869
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-11 21:20:52 +0530

    canvas and window size
    just some ui related changes

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,5 +1,19 @@
+from tkinter import *
+import math
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.minsize(width=600, height=500)
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=300, height=300, highlightthickness=0)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(160, 180, image=logo_img)
+
+canvas.pack()
+
+window.mainloop()
