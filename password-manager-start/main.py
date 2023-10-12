0d4cfa154436e497df20afae5c10c303aed13ec3
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-12 20:10:06 +0530

    password manager
    improving the UI of password generator

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -6,14 +6,33 @@ import math
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.title("Pomodoro")
-window.minsize(width=600, height=500)
+window.title("Password Generator")
 window.config(padx=20, pady=20)
 
-canvas = Canvas(width=300, height=300, highlightthickness=0)
+canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(160, 180, image=logo_img)
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
 
-canvas.pack()
+website_label = Label(text="website")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username")
+email_label.grid(row=2, column=0)
+password_label = Label(text="password")
+password_label.grid(row=3, column=0)
+
+#Entries
+website_entry = Entry(width=56)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=56)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=33)
+password_entry.grid(row=3, column=1)
+
+#Buttons
+generate_password_button = Button(text="Generate Password", width= 18)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=47)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
