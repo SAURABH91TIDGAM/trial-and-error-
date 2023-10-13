ff3e62a385ba312023ef4b407040c94f61eddcd6
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-13 20:55:02 +0530

    saving password website and email ENTRIES

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -3,6 +3,18 @@ import math
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    with open("password_directory.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -32,7 +44,7 @@ password_entry.grid(row=3, column=1)
 #Buttons
 generate_password_button = Button(text="Generate Password", width= 18)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=47)
+add_button = Button(text="Add", width=47, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
