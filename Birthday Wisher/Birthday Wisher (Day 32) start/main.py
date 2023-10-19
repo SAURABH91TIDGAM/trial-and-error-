2aaf0ee9b896db0e71350a34eafb824f33478bb1
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-19 20:12:29 +0530

    birthday wisher
    smtp connection sample mail  sending

---
+++ Birthday Wisher/Birthday Wisher (Day 32) start/main.py
@@ -0,0 +1,12 @@
+import smtplib
+
+my_email = "saurabh1991Tidgam@gmail.com"
+password = "pfwuzmcfoybatwam"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="saurabhtidgam1991@gmail.com",
+        msg="Subject:hello\n\nThis is the body of my email")
