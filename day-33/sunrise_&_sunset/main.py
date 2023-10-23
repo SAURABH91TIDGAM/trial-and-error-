fc2d08eae96b117b5cddd9b9c5b989a11856cda9
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-23 20:59:12 +0530

    sunrise sunset end

--- day-33/sunrise_&_sunset/main.py
+++ day-33/sunrise_&_sunset/main.py
@@ -5,10 +5,12 @@ today = dt.datetime.now()
 date = today.date()
 print(date)
 
-parameters = {"lat": 19.033049, "lng": 73.029663, "date": f"{date}", "formatted": 1}
+parameters = {"lat": 19.033049, "lng": 73.029663, "date": f"{date}", "formatted": 0}
 
 response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 sunrise = response.json()["results"]["sunrise"]
+sunrise = sunrise.split("T")[1].split(":")
 sunset = response.json()["results"]["sunset"]
+sunset = sunset.split("T")[1].split(":")
 
 print(f"sunrise: {sunrise}, sunset: {sunset}")
