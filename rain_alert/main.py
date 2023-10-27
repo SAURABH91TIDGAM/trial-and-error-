c98723a047f4e56edd665cbe91469c72c63b0df2
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-27 20:10:21 +0530

    Update main.py

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,17 +1,41 @@
 import requests
+import os
+from twilio.rest import Client
 
 api_key = "f449e17414d95d80d2c53e8c3efcd728"
-OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather?"
-
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
+account_sid = 'AC4570fa63efab1b73363b811aa4673cd3'
+auth_token = "211248edecae245b8c6e88ee3907500c"
 
 parameters = {
-    "lat": 19.078545,
-    "lon":72.878176,
+    "lat": 6.927079,
+    "lon": 79.861244,
     "appid": api_key
 }
 
 response_received = requests.get(OWM_Endpoint, params=parameters)
+response_received.raise_for_status()
 
-data = response_received.json()
+weather_data = response_received.json()
+weather_slice = weather_data["weather"][0]
 
-print(data)
+print(weather_slice)
+
+
+will_rain = False
+
+for main in weather_slice:
+    condition_code = weather_slice['id']
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    # print("Bring an umbrella.")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It is going to rain today. Remember to bring an Umbrella.",
+        from_='+731613912',
+        to='++918878530091'
+    )
+
