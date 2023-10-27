f776759eacfedd89a38d3aa719bc0eadc58b7757
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-10-27 20:21:33 +0530

    Update main.py

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,6 +1,10 @@
 import requests
 import os
 from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+proxy_client = TwilioHttpClient()
+proxy_client.session.proxies = {'https': os.environ['https_proxy']}
 
 api_key = "f449e17414d95d80d2c53e8c3efcd728"
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
@@ -8,8 +12,8 @@ account_sid = 'AC4570fa63efab1b73363b811aa4673cd3'
 auth_token = "211248edecae245b8c6e88ee3907500c"
 
 parameters = {
-    "lat": 6.927079,
-    "lon": 79.861244,
+    "lat": 25.0330,
+    "lon": 121.5654,
     "appid": api_key
 }
 
@@ -31,7 +35,8 @@ for main in weather_slice:
 
 if will_rain:
     # print("Bring an umbrella.")
-    client = Client(account_sid, auth_token)
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
     message = client.messages \
         .create(
         body="It is going to rain today. Remember to bring an Umbrella.",
