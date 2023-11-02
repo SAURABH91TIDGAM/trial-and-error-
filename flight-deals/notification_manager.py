bb0d0fb4759255f8d794dccdaf0f58557dfb8a87
SaurabhT91 <saurabhtidgam1991@gmail.com>
2023-11-02 21:33:10 +0530

    update flight deals

--- flight-deals/notification_manager.py
+++ flight-deals/notification_manager.py
@@ -1,3 +1,21 @@
+from twilio.rest import Client
+
+TWILIO_SID = 'AC4570fa63efab1b73363b811aa4673cd3'
+TWILIO_AUTH_TOKEN = "211248edecae245b8c6e88ee3907500c"
+TWILIO_VIRTUAL_NUMBER ='+17316137912'
+TWILIO_VERIFIED_NUMBER ='+918878530091'
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
