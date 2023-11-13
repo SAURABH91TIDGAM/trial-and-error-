a980be88949f8795a57144022903245afc53f756
SaurabhT91 <saurabhtidgam1991@gmail.com>
2023-11-13 19:01:29 +0530

    Update notification_manager.py

--- flight-deals/notification_manager.py
+++ flight-deals/notification_manager.py
@@ -19,3 +19,16 @@ class NotificationManager:
         )
         # Prints if successfully sent.
         print(message.sid)
+
+
+
+    def send_emails(self, emails, message):
+        with smtplib.SMTP(EMAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}".encode('utf-8')
+                )
