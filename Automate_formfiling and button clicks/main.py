c0bc0a68e15768268e76f984fb6e0df99291fa0b
SaurabhT91 <saurabhtidgam1991@gmail.com>
2023-12-11 23:56:21 +0530

    Update main.py

--- Automate_formfiling and button clicks/main.py
+++ Automate_formfiling and button clicks/main.py
@@ -1,1 +1,13 @@
-from selenium import webdrier
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_driver_path="C:/Users/saura/.cache/selenium/chromedriver"
+driver = webdriver.Chrome(chrome_driver_path)
+
+driver.get("https://secure-retreat-92358.herokuapp.com")
+
+first_name = driver.find_element(By.Name, "fName")
+first_name.send_keys("Saurabh")
+driver.find_element(By.NAME, "lName")
+driver.find_element(By.NAME, "email")
