e951890a59419c3f7072e06157ad36cff2304d07
SaurabhT91 <saurabhtidgam1991@gmail.com>
2023-12-12 18:52:09 +0530

    update main.py to send elements data and submit

--- Automate_formfiling and button clicks/main.py
+++ Automate_formfiling and button clicks/main.py
@@ -3,11 +3,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path="C:/Users/saura/.cache/selenium/chromedriver"
-driver = webdriver.Chrome(chrome_driver_path)
+driver = webdriver.Chrome()
 
 driver.get("https://secure-retreat-92358.herokuapp.com")
 
-first_name = driver.find_element(By.Name, "fName")
+first_name = driver.find_element(By.NAME, "fName")
 first_name.send_keys("Saurabh")
-driver.find_element(By.NAME, "lName")
-driver.find_element(By.NAME, "email")
+
+last_name = driver.find_element(By.NAME, "lName")
+last_name.send_keys("tidgam")
+
+email = driver.find_element(By.NAME, "email")
+email.send_keys("saurabhtidgam1991@gmail.com")
+
+submit = driver.find_element(By.CSS_SELECTOR, "form button")
+submit.click()
+
+driver.close()
