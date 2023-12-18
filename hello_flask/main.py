a0d628fe0f65a7bff50f486ae5cbb457f3397077
SaurabhT91 <saurabhtidgam1991@gmail.com>
2023-12-18 13:39:43 +0530

    hello flask
    debug mode turned on.

--- hello_flask/main.py
+++ hello_flask/main.py
@@ -1,17 +1,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 @app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
-
+    return '<h1 style="text-align: center">Hello, World!</h1>'\
+            '<p>This is a paragraph</p>'\
+            '<img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGhyOWl3cnNqOHR4bXd2anVvdGFhYWd0dXJvdWh2OXptNm5pYzcyMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gNke2UrUTopOg/giphy.gif">'
 @app.route("/bye")
 def say_bye():
     return "Bye"
 
 @app.route("/username/<name>")
 def greet(name):
-    return f"hello { name}"
+    return f"Hello there { name + 12}"
 
 
 if __name__ == "__main__":
