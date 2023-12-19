35f9d5aee147b36b15ce8531b797d5524d27193a
SaurabhT91 <saurabhtidgam1991@gmail.com>
2023-12-19 19:28:59 +0530

    update

---
+++ JAVAscript/Exercise-1/script.js
@@ -0,0 +1,14 @@
+let js = 'amazing';
+if (js === 'amazing') alert('javascript is FUN!');
+
+
+40 + 35 + 21 - 5;
+console.log(40 + 35 + 21 - 5);
+
+let firstName = 'Matilda';
+let jonas_matilda = "JM";
+let $function = 27;
+
+console.log(firstName);
+console.log(jonas_matilda);
+console.log($function);
