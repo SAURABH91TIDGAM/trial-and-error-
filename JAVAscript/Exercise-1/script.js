ab459faae27b3b7119c44f070a7b103efd3b2537
SaurabhT91 <saurabhtidgam1991@gmail.com>
2023-12-20 19:43:54 +0530

    Update script.js

--- JAVAscript/Exercise-1/script.js
+++ JAVAscript/Exercise-1/script.js
@@ -2,27 +2,36 @@ let js = 'amazing';
 if (js === 'amazing') alert('javascript is FUN!');
 
 
-40 + 35 + 21 - 5;
-console.log(40 + 35 + 21 - 5);
+// 40 + 35 + 21 - 5;
+// console.log(40 + 35 + 21 - 5);
 
 let firstName = 'Matilda';
-let jonas_matilda = "JM";
-let $function = 27;
+// let jonas_matilda = "JM";
+// let $function = 27;
 
 const birthyear = 1991
 var job = "Programmer"
+let year = 2023
 
-console.log(firstName);
-console.log(jonas_matilda);
-console.log($function);
-let javaScriptIsFun = true
-if (javaScriptIsFun) 
-{
-    console.log(javaScriptIsFun);
-    console.log(typeof javaScriptIsFun);
-    console.log(typeof firstName);
-    console.log(typeof jonas_matilda);
-    console.log(typeof $function);
-    job = "Teacher"
-    console.log(job);
-}
+// console.log(firstName);
+// console.log(jonas_matilda);
+// console.log($function);
+// let javaScriptIsFun = true
+// if (javaScriptIsFun)
+// {
+//     console.log(javaScriptIsFun);
+//     console.log(typeof javaScriptIsFun);
+//     console.log(typeof firstName);
+//     console.log(typeof jonas_matilda);
+//     console.log(typeof $function);
+//     job = "Teacher"
+//     console.log(job);
+// }
+
+const jonas = "I'm " + firstName + " a " + (year - birthyear) + 'year old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm  ${ firstName}, a ${year - birthyear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`just a regular string`);
