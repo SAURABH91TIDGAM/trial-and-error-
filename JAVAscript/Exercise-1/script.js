8017a2b491bd57079005b6b29e0ad562cb1a0804
SaurabhT91 <saurabhtidgam1991@gmail.com>
2023-12-23 15:20:09 +0530

    Update script.js
    truthy and false-y

--- JAVAscript/Exercise-1/script.js
+++ JAVAscript/Exercise-1/script.js
@@ -36,20 +36,46 @@ let year = 2023
 
 // console.log(`just a regular string`);
 
-console.log(Number(birthyear), birthyear);
-console.log(Number(birthyear) + 18);
-console.log(Number('jonas'));
-console.log(Number(typeof NaN));
+// console.log(Number(birthyear), birthyear);
+// console.log(Number(birthyear) + 18);
+// console.log(Number('jonas'));
+// console.log(Number(typeof NaN));
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
-console.log('I am ' + 32  + ' years old');
-console.log('23' - '10' - 3); // the result will be 10
-console.log('23' / '2'); // the result will be 11.5
+// console.log('I am ' + 32  + ' years old');
+// console.log('23' - '10' - 3); // the result will be 10
+// console.log('23' / '2'); // the result will be 11.5
 
-let n = '1' + 1; //11  this will not result in 2 it will concatinate the two and make it 11.
-n = n - 1;
-console.log(n);
+// let n = '1' + 1; //11  this will not result in 2 it will concatinate the two and make it 11.
+// n = n - 1;
+// console.log(n);
 
-console.log(2 + 3 + 4 + '5');
-console.log('10'-'4'-'3'-2+'5');
+// console.log(2 + 3 + 4 + '5');
+// console.log('10'-'4'-'3'-2+'5');
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if (money)
+{
+    console.log("Do not spend it all!");
+}
+else
+{
+    console.log("You should get a job!");
+}
+
+let height;
+if (height)
+{
+    console.log("Yay! height is defined!");
+}
+else
+{
+    console.log("Height is UNDEFINED!");    
+}
