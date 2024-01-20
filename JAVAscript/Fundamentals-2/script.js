90f67e1afe96170b88e4227564dcb7253b8f5326
SaurabhT91 <saurabhtidgam1991@gmail.com>
2024-01-20 15:47:55 +0530

    Update script.js

--- JAVAscript/Fundamentals-2/script.js
+++ JAVAscript/Fundamentals-2/script.js
@@ -51,3 +51,13 @@ console.log(age1, age2)
 const calcAge3 = birthYeah => 2037 - birthYeah;
 const age3 = calcAge3(1991);
 console.log(age3)
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+    const age = 2037 - birthYeah;
+    const retirement = 65 - age;
+//     return retirement;
+    return `${firstName} retires in ${retirement} years`;
+ }
+
+console.log(yearsUntilRetirement(1991, 'Saurabh'));
+console.log(yearsUntilRetirement(1991, 'pranjal'));
