82b6a01894fb63d04ccba31d407e4e603bfb4b20
SaurabhT91 <saurabhtidgam1991@gmail.com>
2024-01-20 14:15:24 +0530

    Update script.js

--- JAVAscript/Fundamentals-2/script.js
+++ JAVAscript/Fundamentals-2/script.js
@@ -14,3 +14,34 @@ function loggers() {
 }
 
 loggers();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples, and ${oranges} oranges.`;
+    return juice;
+
+}
+
+const mixedJuice = fruitProcessor(5, 3)
+console.log(mixedJuice);
+const Juice = fruitProcessor(3, 4)
+console.log(Juice);
+
+// Function declaration
+function calcAge1(birthYeah)
+{
+    return 2024 - birthYeah;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1)
+
+
+// Function expression
+const calcage2 = function (birthYeah)
+{
+    return 2024 - birthYeah;
+}
+
+const age2 = calcAge1(1991);
+console.log(age1, age2)
